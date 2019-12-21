ccbb149d2d4cf0b0056a7a0421a13d63fbd05383
Cybertocks <31063326+Cybertocks@users.noreply.github.com>
2019-12-21 07:31:06 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -259,5 +259,5 @@ def form(request):
 
 if __name__ == "__main__":
     # if "serve" in sys.argv:
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="localhost", port=8080)
         # print('hello')
